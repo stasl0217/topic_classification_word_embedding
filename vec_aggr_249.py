f93678aec70fbae26082c82e8eeb73d861eeb2f5
Shirley Chen <sinternoal@msn.com>
2017-03-18 17:25:42 -0400

    add the test part for the kNN classifier. The weight of kNN is set to be 'uniform' to make Euclidean distance equivalent to cosine distance

--- vec_aggr_249.py
+++ vec_aggr_249.py
@@ -4,13 +4,41 @@ from scipy import spatial
 from sklearn.neighbors import KNeighborsClassifier
 
 
+def load_doc_vec(path):
+    vecs = []  # list of entity vector lists
+    X = []  # vectors for every documents (mean of entity vectors)
+
+    for line in open(path):
+        line = line.rstrip('\n').split('\t')
+        docs.append(line)
+
+    print
+    "tags_id loaded"
+
+    count = 1
+    for line in docs:
+        new_vec = []
+        for l in line:
+            v = try_hash(model, l)
+            if v != None:
+                new_vec.append(v)
+        vecs.append(new_vec)
+        if len(new_vec) == 0:
+            print
+            count
+        count += 1
+        X.append(np.mean(new_vec, axis=0))
+
+    print
+    "all mean_vecs:", str(len(X))
+    return X
+
 
 model = Word2Vec.load_word2vec_format(fname='wiki_vec300.bin', binary=True)
 pairs = []
 
 vecs = []  # list of entity vector lists
 X = []  # vectors for every documents (mean of entity vectors)
-w_vecs = []
 docs = []
 
 
@@ -26,38 +54,27 @@ def try_hash(model, word):
 print
 model['Eid1']
 
-for line in open('249train_tags_id.txt'):
-    line = line.rstrip('\n').split('\t')
-    docs.append(line)
+X_train=load_doc_vec('249train_tags_id.txt')
 
-print
-"249_training_tags_id loaded"
-
-count = 1
-for line in docs:
-    new_vec = []
-    for l in line:
-        v = try_hash(model, l)
-        if v != None:
-            new_vec.append(v)
-    vecs.append(new_vec)
-    if len(new_vec) == 0:
-        print
-        count
-    count += 1
-    X.append(np.mean(new_vec, axis=0))
-
-print
-"all mean_vecs:", str(len(mean_vecs))
-
-y = []  # categories
+y_train = []  # categories
 with open('249train_cate.txt') as f_cate:
     for c in f_cate:
         c = int(c.rstrip('\n'))
-        y.append(c)
+        y_train.append(c)
 
 # kNN classification
-neigh = KNeighborsClassifier(n_neighbors=10)
-neigh.fit(X, y)
+neigh = KNeighborsClassifier(n_neighbors=10, weights = 'uniform')
+neigh.fit(X_train, y_train)
 
+X_test=load_doc_vec('249test_tags_id.txt')
+y_test=[]
+with open('249test_cate.txt') as f_cate2:
+    for c in f_cate2:
+        c = int(c.rstrip('\n'))
+        y_test.append(c)
+y_predict=neigh.predict(X_test)
+N_test=len(y_test)
 
+n_error=len(np.nonzero(y_predict-y_test))
+accuracy=1- n_error/N_test
+print accuracy
